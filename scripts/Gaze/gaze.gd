7944d1b6e7b86f13b5daae407b998388910f326c
TenningYaya <98125817+TenningYaya@users.noreply.github.com>
2026-02-20 02:33:50 -0500

    gaze(dragging done)

--- scripts/Gaze/gaze.gd
+++ scripts/Gaze/gaze.gd
@@ -18,9 +18,19 @@ var dragging := false
 var drag_offset := Vector2.ZERO
 
 #旋转角度0/90/180/270
-var rotation := 0
+var rotation_degree := 0
 
-var gaze: Array[Vector2i]
+#整个凝视的集合array（以格子的形式，0,0 这种）
+var gaze: Array[Vector2i] = []
+const shape := {
+	3: [Vector2i(1,1), Vector2i(1,2), Vector2i(2,2)],
+	4: [Vector2i(1,1), Vector2i(1,2), Vector2i(2,1), Vector2i(2,2)],
+	5: [Vector2i(1,1), Vector2i(1,2), Vector2i(2,1), Vector2i(2,2), Vector2i(3,2)],
+	6: [Vector2i(1,1), Vector2i(1,2), Vector2i(2,1), Vector2i(2,2), Vector2i(3,1), Vector2i(3,2)],
+}
+
+var width := 1
+var height := 1
 # 
 func _ready() -> void:
 	pass 
@@ -31,9 +41,34 @@ func _unhandled_input(event: InputEvent) -> void:
 		var mouse := event as InputEventMouseButton
 		#左键按住拖动逻辑
 		if mouse.pressed:
+			if mouse_inside_gaze():
+				dragging = true
+				drag_offset = get_global_mouse_position() - get_top_left()
+				get_viewport().set_input_as_handled()
+		else:
+			dragging = false
 			
 
 #判断鼠标是否在凝视范围里
 func mouse_inside_gaze() -> bool:
-	var mousePos := get_global_mouse_position()
-	var top :=
+	var mousePos := get_global_mouse_position() - get_top_left()
+	#防止0到-1的小数
+	if mousePos.x < 0 or mousePos.y < 0:
+		return false
+	#得到mouse的position处在哪个格子
+	var mouseXPosGridFromTopLeft := int(floor(mousePos.x/grid_size))
+	var mouseYPosGridFromTopLeft := int(floor(mousePos.y/grid_size))
+	#节省算力，先粗略检测
+	if mouseXPosGridFromTopLeft < 0 or mouseYPosGridFromTopLeft < 0 or mouseXPosGridFromTopLeft > width or mouseYPosGridFromTopLeft > height:
+		return false
+	
+	#精确检测，判断在格子里
+	return Vector2i(mouseXPosGridFromTopLeft, mouseYPosGridFromTopLeft) in gaze
+	
+
+func get_top_left() -> Vector2:
+	var size_px := get_pixel_size()
+	return global_position - size_px * 0.5
+
+func get_pixel_size() -> Vector2:
+	return Vector2(float(width * grid_size), float(height * grid_size))
