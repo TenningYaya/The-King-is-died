b3147df325f3825c43995dfe77072603a35daffd
StarsW1nd <zhang.heyan@northeastern.edu>
2026-02-24 00:33:26 -0500

    Combat basic functions

---
+++ scripts/Combat/Enemy_Tank.gd
@@ -0,0 +1,13 @@
+class_name Enemy_Tank
+extends Unit_General
+
+func _on_ready_override() -> void:
+	faction = Faction.ENEMY
+	add_to_group("enemy_units")
+	
+	max_hp = 300.0
+	current_hp = max_hp
+	attack_damage = 15.0
+	attack_range = 60.0
+	move_speed = 50.0
+	attack_type = AttackType.SINGLE
