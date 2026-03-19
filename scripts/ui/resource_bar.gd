75284d5a47d100eaf1b301662db30f16d0318918
april536 <song.zhuor@northeastern.edu>
2026-03-19 09:54:43 +0800

    bug

--- scripts/ui/resource_bar.gd
+++ scripts/ui/resource_bar.gd
@@ -1,4 +1,4 @@
-extends CanvasLayer
+extends Control
 
 # 建立一个字典，把资源 ID 映射到对应的 Label 节点
 # 请确保这些节点路径与你场景树中的实际路径一致
@@ -12,9 +12,7 @@ extends CanvasLayer
 }
 
 func _ready():
-	# 1. 找到场景中的资源管理器（使用了你队友定义的 group: level_manager）
 	var manager = get_tree().get_first_node_in_group("level_manager")
-	
 	if manager:
 		# 2. 连接信号：当资源变动时，执行我们的 _on_resource_changed 函数
 		manager.level_resource_changed.connect(_on_resource_changed)
@@ -25,7 +23,7 @@ func _ready():
 			_update_label_text(res_id, current_val)
 	else:
 		push_error("ResourceBar错误：找不到带有 'level_manager' 分组的节点！")
-
+		
 # 收到信号后的回调函数
 func _on_resource_changed(res_id: String, new_amount: int):
 	_update_label_text(res_id, new_amount)
