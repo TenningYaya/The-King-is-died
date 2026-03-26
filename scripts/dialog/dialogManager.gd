86b47800103b690ed5143c9b41f5c242d06303d6
TenningYaya <98125817+TenningYaya@users.noreply.github.com>
2026-03-26 05:41:31 -0400

    qwq

---
+++ scripts/dialog/dialogManager.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var dialog: DialogueResource
+var tutorial_running := false
+
+func start_tutorial(cue: String = "start") -> void:
+	if tutorial_running:
+		return
+	
+	tutorial_running = true
+	DialogueManager.show_dialogue_balloon(dialog, cue)
+	
+func _ready() -> void:
+	start_tutorial("start")
+	pass # Replace with function body.
